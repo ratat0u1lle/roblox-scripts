3e0bf38d2c99ebe9ccc957612337d662adb43443
ratatouille <70198931+ratat0u1lle@users.noreply.github.com>
2024-07-13 18:07:52 +0200

    Update library.lua

--- library.lua
+++ library.lua
@@ -2911,12 +2911,10 @@ function library:Init(key)
 
             
             selectorLabel.Name = "selectorLabel"
-            selectorLabel.Active = false
-            selectorLabel.Enabled = false
             selectorLabel.Parent = selectorFrame
             selectorLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
             selectorLabel.BackgroundTransparency = 1.000
-            selectorLabel.Size = UDim2.new(0, 396, 0, 24)
+            selectorLabel.Size = UDim2.new(0, 0, 0, 0)
             selectorLabel.Font = Enum.Font.Code
             selectorLabel.Text = text
             selectorLabel.TextColor3 = Color3.fromRGB(190, 190, 190)
